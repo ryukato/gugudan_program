ea370db940c5587f2a2f11a723b7da59a0c03512
anstngus <cpu111846@gmail.com>
2021-05-12 20:26:12 +0900

    [alpha_version]

---
+++ gugudan_program_alpha.py
@@ -0,0 +1,283 @@
+''' 구구단 프로그램 '''
+
+a = 1
+b = 1
+
+print("1단")
+while b < 9:
+    print("1 x 1")
+    print(a * b)
+    b = b + 1
+    print("1 x 2")
+    print(a * b)
+    b = b + 1
+    print("1 x 3")
+    print(a * b)
+    b = b + 1
+    print("1 x 4")
+    print(a * b)
+    b = b + 1
+    print("1 x 5")
+    print(a * b)
+    b = b + 1
+    print("1 x 6")
+    print(a * b)
+    b = b + 1
+    print("1 x 7")
+    print(a * b)
+    b = b + 1
+    print("1 x 8")
+    print(a * b)
+    b = b + 1
+    print("1 x 9")
+    print(a * b)
+    a = a + 1
+b = 1
+
+print("2단")
+while b < 9:
+    print("2 x 1")
+    print(a * b)
+    b = b + 1
+    print("2 x 2")
+    print(a * b)
+    b = b + 1
+    print("2 x 3")
+    print(a * b)
+    b = b + 1
+    print("2 x 4")
+    print(a * b)
+    b = b + 1
+    print("2 x 5")
+    print(a * b)
+    b = b + 1
+    print("2 x 6")
+    print(a * b)
+    b = b + 1
+    print("2 x 7")
+    print(a * b)
+    b = b + 1
+    print("2 x 8")
+    print(a * b)
+    b = b + 1
+    print("2 x 9")
+    print(a * b)
+    a = a + 1
+b = 1
+
+print("3단")
+while b < 9:
+    print("3 x 1")
+    print(a * b)
+    b = b + 1
+    print("3 x 2")
+    print(a * b)
+    b = b + 1
+    print("3 x 3")
+    print(a * b)
+    b = b + 1
+    print("3 x 4")
+    print(a * b)
+    b = b + 1
+    print("3 x 5")
+    print(a * b)
+    b = b + 1
+    print("3 x 6")
+    print(a * b)
+    b = b + 1
+    print("3 x 7")
+    print(a * b)
+    b = b + 1
+    print("3 x 8")
+    print(a * b)
+    b = b + 1
+    print("3 x 9")
+    print(a * b)
+    a = a + 1
+b = 1
+
+print("4단")
+while b < 9:
+    print("4 x 1")
+    print(a * b)
+    b = b + 1
+    print("4 x 2")
+    print(a * b)
+    b = b + 1
+    print("4 x 3")
+    print(a * b)
+    b = b + 1
+    print("4 x 4")
+    print(a * b)
+    b = b + 1
+    print("4 x 5")
+    print(a * b)
+    b = b + 1
+    print("4 x 6")
+    print(a * b)
+    b = b + 1
+    print("4 x 7")
+    print(a * b)
+    b = b + 1
+    print("4 x 8")
+    print(a * b)
+    b = b + 1
+    print("4 x 9")
+    print(a * b)
+    a = a + 1
+b = 1
+
+print("5단")
+while b < 9:
+    print("5 x 1")
+    print(a * b)
+    b = b + 1
+    print("5 x 2")
+    print(a * b)
+    b = b + 1
+    print("5 x 3")
+    print(a * b)
+    b = b + 1
+    print("5 x 4")
+    print(a * b)
+    b = b + 1
+    print("5 x 5")
+    print(a * b)
+    b = b + 1
+    print("5 x 6")
+    print(a * b)
+    b = b + 1
+    print("5 x 7")
+    print(a * b)
+    b = b + 1
+    print("5 x 8")
+    print(a * b)
+    b = b + 1
+    print("5 x 9")
+    print(a * b)
+    a = a + 1
+b = 1
+
+print("6단")
+while b < 9:
+    print("6 x 1")
+    print(a * b)
+    b = b + 1
+    print("6 x 2")
+    print(a * b)
+    b = b + 1
+    print("6 x 3")
+    print(a * b)
+    b = b + 1
+    print("6 x 4")
+    print(a * b)
+    b = b + 1
+    print("6 x 5")
+    print(a * b)
+    b = b + 1
+    print("6 x 6")
+    print(a * b)
+    b = b + 1
+    print("6 x 7")
+    print(a * b)
+    b = b + 1
+    print("6 x 8")
+    print(a * b)
+    b = b + 1
+    print("6 x 9")
+    print(a * b)
+    a = a + 1
+b = 1
+
+print("7단")
+while b < 9:
+    print("7 x 1")
+    print(a * b)
+    b = b + 1
+    print("7 x 2")
+    print(a * b)
+    b = b + 1
+    print("7 x 3")
+    print(a * b)
+    b = b + 1
+    print("7 x 4")
+    print(a * b)
+    b = b + 1
+    print("7 x 5")
+    print(a * b)
+    b = b + 1
+    print("7 x 6")
+    print(a * b)
+    b = b + 1
+    print("7 x 7")
+    print(a * b)
+    b = b + 1
+    print("7 x 8")
+    print(a * b)
+    b = b + 1
+    print("7 x 9")
+    print(a * b)
+    a = a + 1
+b = 1
+
+print("8단")
+while b < 9:
+    print("8 x 1")
+    print(a * b)
+    b = b + 1
+    print("8 x 2")
+    print(a * b)
+    b = b + 1
+    print("8 x 3")
+    print(a * b)
+    b = b + 1
+    print("8 x 4")
+    print(a * b)
+    b = b + 1
+    print("8 x 5")
+    print(a * b)
+    b = b + 1
+    print("8 x 6")
+    print(a * b)
+    b = b + 1
+    print("8 x 7")
+    print(a * b)
+    b = b + 1
+    print("8 x 8")
+    print(a * b)
+    b = b + 1
+    print("8 x 9")
+    print(a * b)
+    a = a + 1
+b = 1
+
+print("9단")
+while b < 9:
+    print("9 x 1")
+    print(a * b)
+    b = b + 1
+    print("9 x 2")
+    print(a * b)
+    b = b + 1
+    print("9 x 3")
+    print(a * b)
+    b = b + 1
+    print("9 x 4")
+    print(a * b)
+    b = b + 1
+    print("9 x 5")
+    print(a * b)
+    b = b + 1
+    print("9 x 6")
+    print(a * b)
+    b = b + 1
+    print("9 x 7")
+    print(a * b)
+    b = b + 1
+    print("9 x 8")
+    print(a * b)
+    b = b + 1
+    print("9 x 9")
+    print(a * b)
+    a = a + 1
+b = 1
