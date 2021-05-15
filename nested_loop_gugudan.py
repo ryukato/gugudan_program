8562b8ba91d0fd0223c4ea8e72a0f8d053ee4b90
ryukato <ryukato79@gmail.com>
2021-05-15 19:49:41 +0900

    chore: polishing

--- nested_loop_gugudan.py
+++ nested_loop_gugudan.py
@@ -3,8 +3,8 @@ j = 1
 while i < 10:
   print(f'================= {i} 단 시작 =================')
   while j < 10:
-    r = i* j
-    print(f'{i} * {j}  ={r}')
+    r = i * j
+    print(f'{i} * {j} = {r}')
     j = j + 1
   i = i + 1
   j = 1
